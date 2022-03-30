d101b32d71ece6ad782e0bee99f8084bb1c0fba9
Gorddd <kasual2014@icloud.com>
2022-03-30 21:29:18 +0300

    Добавьте файлы проекта.

---
+++ Starbucks Calorimeter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Starbucks_Calorimeter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
