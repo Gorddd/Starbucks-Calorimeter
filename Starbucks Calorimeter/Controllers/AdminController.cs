b353ed6e6bd5a02504336a36b74bc186f3796878
Gorddd <kasual2014@icloud.com>
2022-05-11 19:07:30 +0300

    add admincontroller

--- Starbucks Calorimeter/Controllers/AdminController.cs
+++ Starbucks Calorimeter/Controllers/AdminController.cs
@@ -1,14 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Starbucks_Calorimeter.Managers.Users;
+using Starbucks_Calorimeter.Models.Entity;
 
 namespace Starbucks_Calorimeter.Controllers
 {
     public class AdminController : Controller
     {
+        IUserManager manager;
+
+        public AdminController(IUserManager manager)
+        {
+            this.manager = manager;
+        }
+
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(User user)
+        {
+            var admin = manager.GetUser(user.Login, user.Password);
+            if (admin is null)
+                return RedirectToAction(nameof(Login));
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Login),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Login)
+            };
+            var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            await HttpContext.SignInAsync(claimsPrincipal);
+
+            return RedirectToAction(nameof(AdminPanel));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignOutAsync()
+        {
+            await HttpContext.SignOutAsync();
+
+            return RedirectToAction(nameof(Login));
+        }
+
+        [Authorize(Roles = "admin")]
         public IActionResult AdminPanel()
         {
             return View();
