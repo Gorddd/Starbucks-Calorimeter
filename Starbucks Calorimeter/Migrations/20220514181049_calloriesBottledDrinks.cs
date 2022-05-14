7f8760b617d2b44ff1fc3e92fea64f42525e809e
Faleous <myfirsttwink1@gmail.com>
2022-05-14 21:36:34 +0300

    calories for bottled drinks

---
+++ Starbucks Calorimeter/Migrations/20220514181049_calloriesBottledDrinks.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Starbucks_Calorimeter.Migrations
+{
+    public partial class calloriesBottledDrinks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Calories",
+                table: "BottledDrinks",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Carbohidrates",
+                table: "BottledDrinks",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Fats",
+                table: "BottledDrinks",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Proteins",
+                table: "BottledDrinks",
+                type: "float",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Calories",
+                table: "BottledDrinks");
+
+            migrationBuilder.DropColumn(
+                name: "Carbohidrates",
+                table: "BottledDrinks");
+
+            migrationBuilder.DropColumn(
+                name: "Fats",
+                table: "BottledDrinks");
+
+            migrationBuilder.DropColumn(
+                name: "Proteins",
+                table: "BottledDrinks");
+        }
+    }
+}
--- Starbucks Calorimeter/Models/Entity/BottledDrink.cs
+++ Starbucks Calorimeter/Models/Entity/BottledDrink.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public double? Volume { get; set; }
-
+        public double? Proteins { get; set; }
+        public double? Fats { get; set; }
+        public double? Carbohidrates { get; set; }
+        public double? Calories { get; set; }
     }
 }
