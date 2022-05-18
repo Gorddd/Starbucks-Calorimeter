59007811de42719499433104e710012a42b899a6
Gorddd <kasual2014@icloud.com>
2022-05-18 21:40:09 +0300

    add get calories by a drink name

--- Starbucks Calorimeter/Controllers/DrinksController.cs
+++ Starbucks Calorimeter/Controllers/DrinksController.cs
@@ -1,18 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Starbucks_Calorimeter.Managers.Drinks;
 
 namespace Starbucks_Calorimeter.Controllers
 {
     public class DrinksController : Controller
     {
+        IDrinkManager drinkManager;
+
+        public DrinksController(IDrinkManager drinkManager)
+        {
+            this.drinkManager = drinkManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Calories()
+        public IActionResult Calories(string drinkName)
         {
-            return View();
-        }
+            var drink = drinkManager.GetDrink(drinkName);
 
+            return View(drink);
+        }
     }
 }
