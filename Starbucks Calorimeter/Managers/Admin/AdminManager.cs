12bb73d642514561fd3e8e9c9bbc9d58e034bb64
Gorddd <kasual2014@icloud.com>
2022-09-02 23:21:14 +0300

    add AdminManager service

---
+++ Starbucks Calorimeter/Managers/Admin/AdminManager.cs
@@ -0,0 +1,45 @@
+﻿namespace Starbucks_Calorimeter.Managers;
+
+public class AdminManager : IAdminManager
+{
+    public IBottledDrinkManager BottledDrinkManager { get; }
+
+    public ICreamManager CreamManager { get; }
+
+    public IDessertManager DessertManager { get; }
+
+    public IDrinkManager DrinkManager { get; }
+
+    public IEspressoManager EspressoManager { get; }
+
+    public IFoodInPackageManager FoodInPackageManager { get; }
+
+    public ILunchAndBreakfastManager LunchAndBreakfastManager { get; }
+
+    public IMilkManager MilkManager { get; }
+
+    public IPastryManager PastryManager { get; }
+
+    public ISizeManager SizeManager { get; }
+
+    public ISyropManager SyropManager { get; }
+
+    public IUserManager UserManager { get; }
+
+
+    public AdminManager(IApplicationBuilder app)
+    {
+        BottledDrinkManager = app.ApplicationServices.GetRequiredService<IBottledDrinkManager>();
+        CreamManager = app.ApplicationServices.GetRequiredService<ICreamManager>();
+        DessertManager = app.ApplicationServices.GetRequiredService<IDessertManager>();
+        DrinkManager = app.ApplicationServices.GetRequiredService<IDrinkManager>();
+        EspressoManager = app.ApplicationServices.GetRequiredService<IEspressoManager>();
+        FoodInPackageManager = app.ApplicationServices.GetRequiredService<IFoodInPackageManager>();
+        LunchAndBreakfastManager = app.ApplicationServices.GetRequiredService<ILunchAndBreakfastManager>();
+        MilkManager = app.ApplicationServices.GetRequiredService<IMilkManager>();
+        PastryManager = app.ApplicationServices.GetRequiredService<IPastryManager>();
+        SizeManager = app.ApplicationServices.GetRequiredService<ISizeManager>();
+        SyropManager = app.ApplicationServices.GetRequiredService<ISyropManager>();
+        UserManager = app.ApplicationServices.GetRequiredService<IUserManager>();
+    }
+}
--- Starbucks Calorimeter/Managers/Admin/IAdminManager.cs
+++ Starbucks Calorimeter/Managers/Admin/IAdminManager.cs
@@ -2,5 +2,16 @@
 
 public interface IAdminManager
 {
-    
+    IBottledDrinkManager BottledDrinkManager { get; }
+    ICreamManager CreamManager { get; }
+    IDessertManager DessertManager { get; }
+    IDrinkManager DrinkManager { get; }
+    IEspressoManager EspressoManager { get; }
+    IFoodInPackageManager FoodInPackageManager { get; }
+    ILunchAndBreakfastManager LunchAndBreakfastManager { get; }
+    IMilkManager MilkManager { get; } 
+    IPastryManager PastryManager { get; }
+    ISizeManager SizeManager { get; }
+    ISyropManager SyropManager { get; }
+    IUserManager UserManager { get; }
 }
