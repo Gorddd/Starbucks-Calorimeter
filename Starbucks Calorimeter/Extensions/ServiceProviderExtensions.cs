5e18677951d4581f62edf978799f1943f9dd207c
Gorddd <kasual2014@icloud.com>
2022-09-06 23:19:12 +0300

    add starbucks services in separate folder

---
+++ Starbucks Calorimeter/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Starbucks_Calorimeter.Managers;
+
+namespace Starbucks_Calorimeter.Extensions;
+
+public static class ServiceProviderExtensions
+{
+    public static void AddStarbucksServices(this IServiceCollection services)
+    {
+        services.AddSingleton<DrinkView>();
+        services.AddTransient<IUserManager, UserManager>();
+        services.AddTransient<ISizeManager, SizeManager>();
+        services.AddTransient<ISyropManager, SyropManager>();
+        services.AddTransient<ILunchAndBreakfastManager, LunchAndBreakfastManager>();
+        services.AddTransient<IFoodInPackageManager, FoodInPackageManager>();
+        services.AddTransient<IEspressoManager, EspressoManager>();
+        services.AddTransient<IDessertManager, DessertManager>();
+        services.AddTransient<ICreamManager, CreamManager>();
+        services.AddTransient<IBottledDrinkManager, BottledDrinkManager>();
+        services.AddTransient<IMilkManager, MilkManager>();
+        services.AddTransient<IDrinkManager, DrinkManager>();
+        services.AddTransient<IPastryManager, PastryManager>();
+    }
+}
--- Starbucks Calorimeter/Program.cs
+++ Starbucks Calorimeter/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
-using Starbucks_Calorimeter.Managers;
+using Starbucks_Calorimeter.Extensions;
 using Starbucks_Calorimeter.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,19 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddSingleton<DrinkView>();
-builder.Services.AddTransient<IUserManager, UserManager>();
-builder.Services.AddTransient<ISizeManager, SizeManager>();
-builder.Services.AddTransient<ISyropManager, SyropManager>();
-builder.Services.AddTransient<ILunchAndBreakfastManager, LunchAndBreakfastManager>();
-builder.Services.AddTransient<IFoodInPackageManager, FoodInPackageManager>();
-builder.Services.AddTransient<IEspressoManager, EspressoManager>();
-builder.Services.AddTransient<IDessertManager, DessertManager>();
-builder.Services.AddTransient<ICreamManager, CreamManager>();
-builder.Services.AddTransient<IBottledDrinkManager, BottledDrinkManager>();
-builder.Services.AddTransient<IMilkManager, MilkManager>();
-builder.Services.AddTransient<IDrinkManager, DrinkManager>();
-builder.Services.AddTransient<IPastryManager, PastryManager>();
+builder.Services.AddStarbucksServices();
 
 // Создаем контекст. Соединяем с БД
 var connection = builder.Configuration.GetConnectionString("DefaultConnection");
